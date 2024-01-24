7697b032f7b0c95a46b06a9b8145f1c08f4eaec8
Abhirama <01fe22bca034@kletech.ac.in>
2024-01-24 20:35:16 +0530

    This is prime numbers

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,37 +1,37 @@
 import java.util.Scanner;
 
-public class PrimeNumber{
-
+public class PrimeNumber {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the  range: ");
-        int start = s.nextInt();
+        // Input a number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
 
-        System.out.print("Enter the  range: ");
-        int end = s.nextInt();
+        // Check if the number is prime
+        boolean isPrime = isPrimeNumber(number);
 
-        System.out.println("Prime numbers between " + start + " and " + end + ":");
-        generatePrimeNumbers(start, end);
+        // Display the result
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 
-    private static boolean isPrime(int num) {
-        if (num <= 1) {
+    // Method to check if a number is prime
+    public static boolean isPrimeNumber(int n) {
+        if (n <= 1) {
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
                 return false;
             }
         }
         return true;
-    }
-
-    private static void generatePrimeNumbers(int start, int end) {
-        for (int i = start; i <= end; i++) {
-            if (isPrime(i)) {
-                System.out.print(i + " ");
-            }
-        }
     }
 }
